71174411d08948c391a586379c0c98dae1065636
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-04-25 01:24:40 +0530

    URL Bug Fixed

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,6 +1,6 @@
 //Axios Calling Methods.
 import axios from 'axios'
-const url = "http:///localhost:8093/";
+const url = "http://localhost:8093/";
 export const createUser = (user) => axios.post(url + "user", user);
 
 
