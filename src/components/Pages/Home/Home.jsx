3ab3cde796501a60df6e21e3ecd49fc850dac7c2
Taneesha <taneesha.chandrasena@acentura.com>
2021-06-24 19:46:09 +0530

    Rating component completed UI. Keynote speaker on going.

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import VideoLandingComponent from './Sections/VideoLandingPage/VideoLandingComponent';
+import RatingPage from "./Sections/RatingPage/RatingPage";
+import SpeakersPage from "./Sections/SpeakersPage/SpeakersPage";
 
 function Home() {
     return (<React.Fragment>
         <VideoLandingComponent/>
         <p>Recent Activities & Conference</p>
         <p>Recent Activities in Detail</p>
-        <p>Key Note Speakers</p>
+        <RatingPage/>
+        <SpeakersPage/>
         <p>Gallery Page</p>
         <p>Location and Events</p>
     </React.Fragment>)
---
+++ src/components/Pages/Home/Sections/RatingPage/RatingPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Ratings1 from "../../../../Ratings/Ratings1";
+import './ratingstyles.css'
+function RatingPage() {
+    return (<React.Fragment>
+        <div className="rating-section">
+        <div className="rating-header">
+            <h2 className="rating-header-main-header">Register</h2>
+            <h4 className="rating-header-second-header">Information</h4>
+        </div>
+        <div className="cardLayout">
+            <Ratings1/>
+            <div className="line"></div>
+            <Ratings1/>
+            <div className="line"></div>
+            <Ratings1/>
+        </div>
+        </div>
+
+
+    </React.Fragment>)
+}
+
+export default RatingPage;
