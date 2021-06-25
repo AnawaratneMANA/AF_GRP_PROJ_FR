0f593e0fc0fae133921369e09f787666c7ab825a
Taneesha <taneesha.chandrasena@acentura.com>
2021-06-25 23:19:45 +0530

    Key Speaker component with Key Speakers Page Completed

--- src/components/KeySpeakers/KeySpeaker1.jsx
+++ src/components/KeySpeakers/KeySpeaker1.jsx
@@ -1,12 +1,19 @@
 import React, {useState} from 'react';
+import './Speakers.css';
+import Speaker from './SpeakersImges/speaker.jpg'
+
 
 
 
 function KeySpeaker1(){
     return(
-        <div className="rating">
-            <div className="rating_info">
-
+        <div className="speaker">
+            <div className="img_top">
+                <img className='photo' src={Speaker}></img>
+            </div>
+            <div className="speaker_info">
+                <h3>Akash</h3>
+                <p>Bsc.Software Engineering</p>
             </div>
         </div>
     );
