a40a7f6c0963cb1e7cfeb5d13f9dc2875ecd06fb
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-06-29 10:52:10 +0530

    Data fetching successful.

--- src/components/Pages/Home/Sections/DownloadPage/DownloadLinkPage.jsx
+++ src/components/Pages/Home/Sections/DownloadPage/DownloadLinkPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {  Link } from 'react-router-dom';
 import './downloadstyles.css';
 import UploadForm from "./UploadForm/UploadForm";
+import FileGrid from "./UploadForm/FileGrid";
 function DownloadLinkPage(props) {
     return (
         <div className ="downloadlinkpage">
@@ -10,6 +11,7 @@ function DownloadLinkPage(props) {
             <h1>File Attachment Form</h1>
             <form>
                 <UploadForm/>
+                <FileGrid/>
             </form>
 
 
---
+++ src/components/Pages/Home/Sections/DownloadPage/UploadForm/FileGrid.js
@@ -0,0 +1,25 @@
+import React  from "react";
+import useFirestore from "../../../../../../Firebase/useFirestore";
+
+const FileGrid = () => {
+
+    const { docs } = useFirestore('images');
+    console.log(docs);
+
+        return (
+            <div className="file-grid">
+                {docs && docs.map(doc => (
+                    <table>
+                    <div className="file-wrap" key={doc.id}>
+                        {/*<img src={doc.url} alt="uploaded file"/>*/}
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </div>
+                    </table>
+                ))}
+            </div>
+        )
+}
+
+export default FileGrid;
