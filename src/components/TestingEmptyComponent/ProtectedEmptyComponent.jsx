0fac43e8a09ea00ff1f7bb1aa588ad7437dea771
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-06-29 13:23:58 +0530

    Added a Empty Component to the Project.

---
+++ src/components/TestingEmptyComponent/ProtectedEmptyComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const LoginPage = () => {
+    return (
+        <div className="loginPage">
+            This is a Empty Component Do Not add Data.
+        </div>
+    )
+}
+export default LoginPage;
