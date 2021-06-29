01fb37ad0ece087f3e22a0c95317fefe94e0501c
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-06-29 10:32:43 +0530

    Firestore created and plug all the files to a table.

--- src/Firebase/useFirestore.js
+++ src/Firebase/useFirestore.js
@@ -4,12 +4,22 @@ import { projectFirestore } from "./config";
 const useFirestore = (collection) => {
     const [docs, setDocs] = useState([]);
 
-    useEffect(()=> {
-        projectFirestore.collection(collection)
+    useEffect(() => {
+        const unsub = projectFirestore.collection(collection)
+            .orderBy("createdAt", 'desc')
             .onSnapshot((snap) => {
+               let document = [];
+               snap.forEach(doc => {
+                   document.push({...doc.data(), id: doc.id })
+               });
+               setDocs(document);
+            });
 
-            },[])
-        return { docs };
-    })
+        return () => unsub();
+
+    }, [collection])
+    return { docs };
 }
+
+export default useFirestore;
 
--- src/components/Pages/Home/Sections/DownloadPage/UploadForm/ImageGrid.js
+++ src/components/Pages/Home/Sections/DownloadPage/UploadForm/ImageGrid.js
@@ -1,10 +1,11 @@
 import {Title} from "@devexpress/dx-react-chart";
 import UploadForm from "./UploadForm";
+import useFirestore from "../../../../../../Firebase/useFirestore";
 
 function ImageGrid() {
     const ImageGrid = () => {
         return (
-            <div className="file-attachment">
+            <div className="file-grid">
                 <Title/>
                 <UploadForm/>
                 <ImageGrid/>
