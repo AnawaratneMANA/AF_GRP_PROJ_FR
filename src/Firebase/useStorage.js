056956a9ef773450c9051be46d21dcf04aecbd25
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-06-29 08:35:25 +0530

    Created a Custom Hook to save file on the Firebase server.

---
+++ src/Firebase/useStorage.js
@@ -0,0 +1,26 @@
+import { useState, useEffect} from 'react';
+import { projectStorage } from './config';
+
+const useStorage = (file) =>{
+    const [progress, setProgress] = useState(0);
+    const [error, setError] = useState(null);
+    const [url, setUrl] = useState(null);
+
+    useEffect(() => {
+        // ref
+        const storageRef = projectStorage.ref(file.name);
+        storageRef.put(file).on('state_changed',(snap)=> {
+            let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+            setProgress(percentage);
+        }, (error) => {
+            setError(error);
+        }, async () => {
+            const url = await storageRef.getDownloadURL();
+            setUrl(url);
+        });
+    }, [file])
+
+    return {progress, url, error}
+}
+
+export default useStorage;
