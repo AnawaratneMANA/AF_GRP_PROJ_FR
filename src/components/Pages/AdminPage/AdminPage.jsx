c4ad3cc1411c2fe21a6e0521270f598775a6d531
MDilankaN <Dilankan1998@gmail.com>
2021-07-03 01:15:17 +0530

    Dyanamic collapes Componet

--- src/components/Pages/AdminPage/AdminPage.jsx
+++ src/components/Pages/AdminPage/AdminPage.jsx
@@ -8,14 +8,56 @@ import EventGraph from "./AdminComponents/EventGraph";
 import AdminFileTable from "./AdminComponents/AdminFileTable";
 import CollapsComponent from "./CollapsComponent/CollapsComponent";
 import {useSelector} from "react-redux";
+import useFirestore from "../../../Firebase/useFirestore";
+import axios from "axios";
 
 const AdminPage = () => {
     const users = useSelector((state) => state.users);
     const [flag, setFlag] = useState(null)
     const [tableUser, setTableUser] = useState([]);
     const [isSubmited, setIsSubmitted] = useState(false);
-    const [eventArray, setEventArray] = useState([]);
+    const [eventArray, setEventArray] = useState();
+    const [userData, setuserData] = useState([]);
     //Add this to protected Components to avoid unauthorized users from comming.
+
+    const { docs } = useFirestore('images');
+    let totaldown = docs.length;
+
+    useEffect(() => {
+        fetchUserDetails();
+    }, [])
+
+    const fetchUserDetails = async () => {
+        try {
+            const response = await axios
+                .get("http://localhost:8093/api/v1/users")
+                .catch((error) => {
+                    console.log("Error", error);
+                });
+            setuserData(response.data)
+
+        } catch (err) {
+            console.log("Error");
+            console.log(err.message);
+        }
+    }
+
+
+
+    const data = [{
+        id: 1,
+        Heading: "Total Files",
+        description:"Number of total file uploads in the website",
+        Total: totaldown
+    },{
+        id: 2,
+        Heading: "Total Users",
+        description:"Total Users in the website",
+        Total: userData.length
+    }]
+
+    console.log(data);
+
     useEffect(()=> {
         if(users.userType != "Admin"){
             window.location.href='/loginpage';
@@ -28,6 +70,8 @@ const AdminPage = () => {
         return null;
     }
 
+
+
     return (<div>
             
             <div className="adminInterface">
@@ -35,6 +79,12 @@ const AdminPage = () => {
                     <div className="admin__row">
                     <h1> Monitoring Section </h1>
                         {/*<CollapsComponent/>*/}
+                    </div>
+                    <div className="admin__row1">
+                        {data.map((value) => (
+                            <CollapsComponent value = {value}/>
+                            ))}
+
                     </div>
                     <div className="admin__row">
                         {/*<AdminStatistics/>*/}
