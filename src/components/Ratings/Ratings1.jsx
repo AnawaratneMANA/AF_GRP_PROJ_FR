a67021970e88fffe0bba8424fb41af19dd574b7c
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-07-02 22:38:34 +0530

    Merging Feedback DB implementations

--- src/components/Ratings/Ratings1.jsx
+++ src/components/Ratings/Ratings1.jsx
@@ -6,7 +6,6 @@ function Ratings1({rows}){
     return(
       <div className="rating">
           <div className="img_top1">
-              <img className='photo1' src={rows.image}></img>
           </div>
           <div className="rating_info">
               <h3>{rows.title}</h3>
